c5c8d814c161e0bbcf4282049ee3a1a8fa9f265c
yadimashar2013 <162845121+yadimashar2013@users.noreply.github.com>
2024-05-09 08:15:42 +0700

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,16 @@
+team1_num = 5
+team2_num = 6
+print("В команде Мастера кода участников: %d ! " % (team1_num))
+print("Итого сегодня в командах участников: %d и %d !" % (team1_num, team2_num))
+score_2 = 18
+print("Команда Волшебники данных решила задач: {} !".format(score_2))
+team1_time = 56
+print(" Волшебники данных решили задачи за {} c. !".format(team1_time))
+score_1 = 14
+print(f"Команды решили {score_1} и {score_2} задач.")
+challenge_result = 8
+result = "Результат битвы: победа команды Мастера кода!"
+print(result, f"С результатом {challenge_result} баллов.")
+tasks_total = 32
+time_avg = 14.23
+print(f"Сегодня было решенно {tasks_total} задач, в среднем по {time_avg} секунды на задачу!.")
